43be881d5cf7f51298617c00a203d488773b9a3b
Rohit Bansal <rohit@bansalrohit.com>
2016-02-10 21:01:52 -0800

    Initial commit

---
+++ HIDKeyboard.ino
@@ -0,0 +1,18 @@
+/*
+  HID Keyboard
+
+  This contains arduino code to use an ATTiny85 to work as an HID keyboard. 
+
+ */
+
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin 0 as an output.
+  pinMode(0, OUTPUT);
+  digitalWrite(0, HIGH);   // turn the LED on to indicate its on
+}
+
+// the loop function runs over and over again forever
+void loop() {
+}
